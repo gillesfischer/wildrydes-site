5f5e28755c128c3d254f1ad209940b71e75ad7fb
Gilles Fischer <gilles.fischer@gmail.com>
2026-09-25 16:23:59 +0100

    Update 2 config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_gh987uyWJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7iu7ds06uajls1ilbmk3lnj31k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_pc58Nvytz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '671a75vsjdntm7cvj77igm7r3l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
